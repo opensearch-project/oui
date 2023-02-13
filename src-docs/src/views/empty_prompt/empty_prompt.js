b1e9116fcf2907c9c5e92180d7f8c94919110739
AbhishekReddy1127 <nallamsa@amazon.com>
2023-02-13 18:39:11 +0000

    Updated the content for the Empty prompt
    Signed-off-by: AbhishekReddy1127 <nallamsa@amazon.com>

--- src-docs/src/views/empty_prompt/empty_prompt.js
+++ src-docs/src/views/empty_prompt/empty_prompt.js
@@ -15,21 +15,22 @@ import { OuiEmptyPrompt, OuiButton } from '../../../../src/components';
 
 export default () => (
   <OuiEmptyPrompt
-    iconType="editorStrike"
-    title={<h2>You have no spice</h2>}
+    iconType="database"
+    title={<h2>No data available</h2>}
     body={
       <Fragment>
         <p>
-          Navigators use massive amounts of spice to gain a limited form of
-          prescience. This allows them to safely navigate interstellar space,
-          enabling trade and travel throughout the galaxy.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Donec accumsan, nulla sed blandit semper.
         </p>
-        <p>You&rsquo;ll need spice to rule Arrakis, young Atreides.</p>
+        <p>
+          tell&rsquo;us est convallis mauris, eget consequat mi lacus non ante.
+        </p>
       </Fragment>
     }
     actions={
       <OuiButton color="primary" fill>
-        Harvest spice
+        Connect to a data source
       </OuiButton>
     }
   />
