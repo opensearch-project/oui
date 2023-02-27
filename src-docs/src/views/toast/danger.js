f551bc1ca3c388a0ba26ed707477a0490b6fccab
AbhishekReddy1127 <nallamsa@amazon.com>
2023-02-27 04:14:59 +0000

    Updated the error message
    Signed-off-by: AbhishekReddy1127 <nallamsa@amazon.com>

--- src-docs/src/views/toast/danger.js
+++ src-docs/src/views/toast/danger.js
@@ -14,8 +14,7 @@ import React from 'react';
 import { OuiToast } from '../../../../src/components';
 
 const esError =
-  'Error: expected_scroll_id in the following response: {"took":0,"timed_out":false,"_shards":{"total":0,"successful":0,"skipped":0,"failed":0},"hits":{"total":0,"max_score":0,"hits":[]}}';
-
+  'Error: expected _scroll_id in the following response: {"took":0,"timed_out":false,"_shards":{"total":0,"successful":0,"skipped":0,"failed":0},"hits":{"total":0,"max_score":0,"hits":[]}}';
 export default () => (
   <OuiToast
     title="Couldn't complete the search"
