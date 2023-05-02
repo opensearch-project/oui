6043f7c0009b16f13634dd6e3199e043a17524e7
Andrey Myssak <40265277+andreymyssak@users.noreply.github.com>
2023-05-03 01:06:12 +0600

    Update @svgr/core and @svgr/plugin-svgo (#594) (#649)
    Signed-off-by: Andrey Myssak <andreymyssak@gmail.com>
    Co-authored-by: Sergey Myssak <sergey.myssak@gmail.com>

--- src/components/icon/assets/radius.js
+++ src/components/icon/assets/radius.js
@@ -10,22 +10,20 @@
  */
 
 import * as React from 'react';
-
 const OuiIconRadius = ({ title, titleId, ...props }) => (
   <svg
+    xmlns="http://www.w3.org/2000/svg"
     width={16}
     height={16}
     viewBox="0 0 16 16"
-    xmlns="http://www.w3.org/2000/svg"
     aria-labelledby={titleId}
     {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path
       fillRule="evenodd"
+      d="M3.05 12.95a7 7 0 1 0 9.9-9.9 7 7 0 0 0-9.9 9.9Zm.707-.707a6 6 0 0 0 8.825-8.118L10 6.707V10H6V6h3.293l2.582-2.582a6 6 0 0 0-8.118 8.825ZM9 7v2H7V7h2Z"
       clipRule="evenodd"
-      d="M3.05 12.95a7 7 0 109.9-9.9 7 7 0 00-9.9 9.9zm.707-.707a6 6 0 008.825-8.118L10 6.707V10H6V6h3.293l2.582-2.582a6 6 0 00-8.118 8.825zM9 7v2H7V7h2z"
     />
   </svg>
 );
-
 export const icon = OuiIconRadius;
