bd34706c56e45109ae5decf38303ae34e0c66aec
Andrey Myssak <andreymyssak@gmail.com>
2023-05-29 15:50:56 +0600

    Remove import/no-unresolved from .eslintrc.js (#584)
    Co-authored-by: Sergey Myssak <sergey.myssak@gmail.com>
    Signed-off-by: Andrey Myssak <andreymyssak@gmail.com>

--- src-docs/webpack.config.js
+++ src-docs/webpack.config.js
@@ -16,8 +16,6 @@ const babelConfig = require('./.babelrc.js');
 // const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-const getPort = require('get-port');
-
 const { NODE_ENV, CI, WEBPACK_SERVE } = process.env;
 
 const isDevelopment = WEBPACK_SERVE === 'true' && CI == null;
@@ -40,133 +38,126 @@ function employCache(loaders) {
   return loaders;
 }
 
-const webpackConfig = async () => {
-  const port = await getPort({
-    port: getPort.makeRange(8030, 8130),
-    host: '0.0.0.0',
-  });
+const webpackConfig = {
+  mode: isProduction ? 'production' : 'development',
 
-  return {
-    mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
 
-    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+  entry: {
+    bundle: './index.js',
+  },
 
-    entry: {
-      bundle: './index.js',
+  context: path.resolve(__dirname, 'src'),
+
+  output: {
+    path: path.resolve(__dirname, '../docs'),
+    filename: `[name]${isProduction ? '.min' : ''}.js`,
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    fallback: {
+      fs: false,
     },
+  },
 
-    context: path.resolve(__dirname, 'src'),
-
-    output: {
-      path: path.resolve(__dirname, '../docs'),
-      filename: `[name]${isProduction ? '.min' : ''}.js`,
+  resolveLoader: {
+    alias: {
+      'prop-loader': path.resolve(
+        __dirname,
+        '../scripts/loaders/prop-loader.js'
+      ),
     },
+  },
 
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json'],
-      fallback: {
-        fs: false,
+  module: {
+    rules: [
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source',
       },
-    },
-
-    resolveLoader: {
-      alias: {
-        'prop-loader': path.resolve(
-          __dirname,
-          '../scripts/loaders/prop-loader.js'
-        ),
+      {
+        test: /\.(js|tsx?)$/,
+        use: employCache([
+          {
+            loader: 'babel-loader',
+            options: { babelrc: false, ...babelConfig },
+          },
+        ]),
+        exclude: [/node_modules/, /packages(\/|\\)react-datepicker/],
       },
-    },
-
-    module: {
-      rules: [
-        {
-          resourceQuery: /raw/,
-          type: 'asset/source',
-        },
-        {
-          test: /\.(js|tsx?)$/,
-          use: employCache([
-            {
-              loader: 'babel-loader',
-              options: { babelrc: false, ...babelConfig },
-            },
-          ]),
-          exclude: [/node_modules/, /packages(\/|\\)react-datepicker/],
-        },
-        {
-          test: /\.scss$/,
-          use: employCache([
-            {
-              loader: 'style-loader',
-              options: { injectType: 'lazySingletonStyleTag' },
-            },
-            'css-loader',
-            'postcss-loader',
-            'sass-loader',
-          ]),
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: employCache(['style-loader', 'css-loader']),
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(woff|woff2|ttf|eot|ico)(\?|$)/,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.(png|jp(e*)g|svg|gif)$/,
-          type: 'asset',
-          generator: {
-            filename: 'images/[contenthash]-[name].[ext]',
+      {
+        test: /\.scss$/,
+        use: employCache([
+          {
+            loader: 'style-loader',
+            options: { injectType: 'lazySingletonStyleTag' },
           },
-          parser: {
-            dataUrlCondition: {
-              maxSize: 8 * 1024,
-            },
-          },
-        },
-      ],
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'index.html',
-        inject: 'body',
-        cache: true,
-        showErrors: true,
-      }),
-
-      new CircularDependencyPlugin({
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]),
         exclude: /node_modules/,
-        failOnError: true,
-      }),
-
-      new NodePolyfillPlugin(),
-      // run TypeScript during webpack build
-      // new ForkTsCheckerWebpackPlugin({
-      //   typescript: { configFile: path.resolve(__dirname, '..', 'tsconfig.json') },
-      //   async: false, // makes errors more visible, but potentially less performant
-      // }),
-    ],
-
-    devServer: isDevelopment
-      ? {
-          static: {
-            directory: path.resolve(__dirname, 'build'),
-            // prevent file watching while running on CI
-            // /app/ represents the entire docker environment
-            watch: isPuppeteer ? { ignored: '**/*' } : undefined,
+      },
+      {
+        test: /\.css$/,
+        use: employCache(['style-loader', 'css-loader']),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|ico)(\?|$)/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        type: 'asset',
+        generator: {
+          filename: 'images/[contenthash]-[name].[ext]',
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
           },
-          host: '0.0.0.0',
-          port,
-          allowedHosts: 'all',
-          historyApiFallback: true,
-        }
-      : undefined,
-  };
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      cache: true,
+      showErrors: true,
+    }),
+
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
+    }),
+
+    new NodePolyfillPlugin(),
+    // run TypeScript during webpack build
+    // new ForkTsCheckerWebpackPlugin({
+    //   typescript: { configFile: path.resolve(__dirname, '..', 'tsconfig.json') },
+    //   async: false, // makes errors more visible, but potentially less performant
+    // }),
+  ],
+
+  devServer: isDevelopment
+    ? {
+        static: {
+          directory: path.resolve(__dirname, 'build'),
+          // prevent file watching while running on CI
+          // /app/ represents the entire docker environment
+          watch: isPuppeteer ? { ignored: '**/*' } : undefined,
+        },
+        host: '0.0.0.0',
+        port: 8030,
+        allowedHosts: 'all',
+        historyApiFallback: true,
+      }
+    : undefined,
 };
 
 module.exports = webpackConfig;
