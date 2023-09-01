9c5123a3863a9299418894117c1563ccab4ae1d6
Josh Romero <rmerqg@amazon.com>
2023-09-01 21:02:40 +0000

    Remove explicit height and width properties
    Signed-off-by: Josh Romero <rmerqg@amazon.com>

--- src/components/icon/assets/checkInCircleEmpty.js
+++ src/components/icon/assets/checkInCircleEmpty.js
@@ -13,8 +13,6 @@ import * as React from 'react';
 const OuiIconCheckInCircleEmpty = ({ title, titleId, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={16}
-    height={16}
     viewBox="0 0 16 16"
     aria-labelledby={titleId}
     {...props}>
