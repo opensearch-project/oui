cc445de2f11c9b8239d710133673ddc534d73b8f
Shashwat Prakash <88332189+AwesomeSauce42@users.noreply.github.com>
2023-10-16 15:05:04 +0530

    Update compile-oui.js
    Signed-off-by: Shashwat Prakash <88332189+AwesomeSauce42@users.noreply.github.com>

--- scripts/compile-oui.js
+++ scripts/compile-oui.js
@@ -158,24 +158,6 @@ function euiBuildTimeAliasTypeDef(file) {
   fs.writeFileSync(file, '\n' + reExportStatements.join('\n'), { flag: 'a', encoding: 'utf8' });
 }
 
-// function euiBuildTimeAliasTearDown() {
-//   console.log('Tearing down build-time EUI aliases');
-//   shell.rm('-rf', 'src/eui_components');
-
-//   // Remove any changes made at build time
-//   shell.ls('src/components/**/*.*').forEach(file => {
-//     if (/__snapshots__|\.d\.ts|\.test\.|\.scss/.test(file)) return;
-//     let changed;
-//     const content = fs.readFileSync(file, 'utf8')
-//       .replace(/\n\/\* OUI -> EUI Aliases: Build-Time \*\/.*$/mg, () => {
-//         changed = true;
-//         return '';
-//       });
-//     if (changed) fs.writeFileSync(file, content, 'utf8');
-//   });
-// }
-/* End of Aliases */
-
 function compileLib() {
   shell.mkdir(
     '-p',
