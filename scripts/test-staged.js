6dcd4f0028c8ccbfa0a39813c7ba6720e3d0f944
Willie Hung <willie880201044@gmail.com>
2023-10-24 18:24:04 -0500

    Revise to  a more concise way for removing empty entries
    Signed-off-by: Willie Hung <willie880201044@gmail.com>

--- scripts/test-staged.js
+++ scripts/test-staged.js
@@ -12,17 +12,14 @@
 const { execSync } = require('child_process');
 
 // find names of staged files
-const stagedFiles = execSync(
-  'git diff --cached --name-only --diff-filter=ACDMR'
-)
+const stagedFiles = execSync('git diff --cached --name-only --diff-filter=ACMR')
   .toString()
-  .split(/[\r\n]+/g);
-
-if (stagedFiles.length === 1 && stagedFiles[0] === '') {
-  process.exit(0);
-}
+  .split(/[\r\n]+/g)
+  .filter(Boolean); // remove empty entries
 
 // execute tests related to the staged files
-execSync(`yarn test-unit --findRelatedTests ${stagedFiles.join(' ')}`, {
-  stdio: 'inherit',
-});
+if (stagedFiles.length > 0) {
+  execSync(`yarn test-unit --findRelatedTests ${stagedFiles.join(' ')}`, {
+    stdio: 'inherit',
+  });
+}
