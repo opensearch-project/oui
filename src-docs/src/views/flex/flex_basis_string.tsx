ceed1ceb8b1a1c85af3e9ec4a91cb13dc71d2a4b
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-01-12 12:25:29 -0300

    updating text for examples of Basis in flex items
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src-docs/src/views/flex/flex_basis_string.tsx
+++ src-docs/src/views/flex/flex_basis_string.tsx
@@ -11,23 +11,27 @@ export default () => (
   <div>
     <OuiFlexGroup>
       <OuiFlexItem basis={'auto'}>
-        Auto
+        auto
+        <br />
         <br />
         Varies based on content size or width/height property
       </OuiFlexItem>
       <OuiFlexItem basis={'fit-content'}>
-        Fit-content
+        fit-content
+        <br />
         <br />
         Sizes the item based on the content&apos;s width but within the
         container.
       </OuiFlexItem>
       <OuiFlexItem basis={'max-content'}>
-        Max-content
+        max-content
+        <br />
         <br />
         Sizes the item to be as large as the content&apos;s maximum width.
       </OuiFlexItem>
       <OuiFlexItem basis={'min-content'}>
-        Min-content
+        min-content
+        <br />
         <br />
         Sizes the item to be as small as the content&apos;s minimum width
         without breaking.
